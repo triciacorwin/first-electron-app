46c8b92c3d0a30d863cea142a79e69a632bfb795
Tricia Yeh <triciayeh@Tricias-MacBook-Air.local>
2023-06-29 23:09:41 -0700

    set up app with renderer and debug script from VS code

--- main.js
+++ main.js
@@ -1,1 +1,30 @@
-console.log('Hello from Electron😅');
+// importing modules with CommmonJS module syntax
+  // app: control your application's event lifecycle
+  // BrowserWindow: create and manage app windows
+const { app, BrowserWindow } = require('electron');
+
+// load your web page into a new BrowserWindow instance
+const createWindow = () => {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600
+  })
+
+  win.loadFile('index.html')
+}
+
+// run function when app is ready (Mac)
+app.whenReady().then(() => {
+  createWindow();
+
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createWindow();
+    }
+  })
+})
+
+// quit app whne all windows are closed
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') app.quit;
+}) 
